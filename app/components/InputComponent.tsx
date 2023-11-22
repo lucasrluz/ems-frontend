239246592ab68d461e133af87cf8ed7946ce8ee8
lucasrluz <lucasr.luz42@gmail.com>
2023-11-22 17:31:54 -0300

    feat: adicionado novas propriedades para InputComponent

--- app/components/InputComponent.tsx
+++ app/components/InputComponent.tsx
@@ -1,9 +1,11 @@
-export function InputComponenet({ placeholder }: { placeholder: string }) {
+export function InputComponenet({ placeholder, value, password }: { placeholder: string, value?: string, password?: boolean }) {
   return (
     <input
       className="mt-4 h-11 w-96 rounded-lg border border-slate-300 pl-4"
       type="text"
       placeholder={placeholder}
+	  value={value}
+	  type={password ? "password" : "text"}
     />
   );
 }
